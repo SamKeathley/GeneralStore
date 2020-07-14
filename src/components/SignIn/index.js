d3086290be16c5e21c2a040a90419fce79b9b508
Sam Keathley <samncollette@gmail.com>
2020-07-13 18:25:50 -0700

    set up signup and signin

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import Button from './../Forms/Button';
+import FormInput from './../Forms/FormInput';
 import { signInWithGoogle } from './../../firebase/utils';
 
 function SignIn(props) {
+    const [userSignIn, setUserSignIn] = useState({
+        email: '',
+        password: ''
+    });
+
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        e.preventDefault();
+        setUserSignIn(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            };
+        });
+    }
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -17,6 +34,27 @@ function SignIn(props) {
                 </h2>
                 <div className="formWrap">
                     <form onSubmit={handleSubmit}>
+
+                        <FormInput
+                            type="email"
+                            name="email"
+                            value={userSignIn.email}
+                            placeholder="Email"
+                            onChange={handleChange}
+                        />
+
+                        <FormInput
+                            type="password"
+                            name="password"
+                            value={userSignIn.password}
+                            placeholder="Password"
+                            onChange={handleChange}
+                        />
+
+                        <Button type="submit">
+                            LogIn
+                        </Button>
+
                         <div className="socialSignin">
                             <div className="row">
                                 <Button onClick={signInWithGoogle}>
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -5,22 +5,36 @@ import FormInput from './../Forms/FormInput';
 import Button from './../Forms/Button';
 
 function SignUp() {
-    const [emailSignUp, setEmailSignUp] = useState({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
+    const [displayName, setDisplayName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
-    function handleChange(e) {
-        const { name, value } = e.target;
+    function reset() {
+        setDisplayName('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+        setErrors([]);
+    }
 
-        setEmailSignUp(prevValue => {
-            return {
-                ...prevValue,
-                [name]: value
-            };
-        });
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            const err = ['Passwords Do Not Match!'];
+            setErrors(err)
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+            await handleUserProfile(user, { displayName });
+            reset();
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
@@ -30,35 +44,47 @@ function SignUp() {
                     Signup
                 </h2>
                 <div className="formWrap" >
-                    <form>
+
+                    {errors.length > 0 && (
+                        <ul>
+                            {errors.map((err, index) => {
+                                return (
+                                    <li key={index}>
+                                        {err}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+                    <form onSubmit={handleFormSubmit}>
                         <FormInput
                             type="text"
                             name="displayName"
-                            value={emailSignUp.displayName}
+                            value={displayName}
                             placeholder="Full Name"
-                            onChange={handleChange}
+                            handleChange={e => setDisplayName(e.target.value)}
                         />
                         <FormInput
                             type="email"
                             name="email"
-                            value={emailSignUp.email}
+                            value={email}
                             placeholder="Email Address"
-                            onChange={handleChange}
+                            handleChange={e => setEmail(e.target.value)}
                         />
                         <FormInput
                             type="password"
                             name="password"
-                            value={emailSignUp.password}
+                            value={password}
                             placeholder="Password"
-                            onChange={handleChange}
+                            handleChange={e => setPassword(e.target.value)}
                         />
 
                         <FormInput
                             type="password"
                             name="confirmPassword"
-                            value={emailSignUp.confirmPassword}
+                            value={confirmPassword}
                             placeholder="Confirm Password"
-                            onChange={handleChange}
+                            handleChange={e => setConfirmPassword(e.target.value)}
                         />
 
                         <Button type="submit">
